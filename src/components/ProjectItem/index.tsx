ea73ce0d15aae98617ad17c3aa695aeea197d565
George-b1t <george.soares509@gmail.com>
2022-12-21 17:34:29 -0300

    Blurhash adicionado às imagens dos projetos

--- src/components/ProjectItem/index.tsx
+++ src/components/ProjectItem/index.tsx
@@ -1,3 +1,4 @@
+import { Blurhash } from "react-blurhash";
 import styles from "./styles.module.scss";
 
 export interface ProjectProps {
@@ -5,6 +6,7 @@ export interface ProjectProps {
   name: string;
   description: string;
   image: string;
+  hashImage: string;
   starred: boolean;
 }
 
@@ -24,6 +26,12 @@ function ProjectItem({ setIsOpen, setProject, index, item }: ProjectItem) {
   return (
     <div className={styles.container} style={{ animationDelay: `${index*100}ms` }} onClick={openForm}>
       <h3 className={styles.title}>{item.name}</h3>
+      <Blurhash
+        hash={item.hashImage}
+        width={330}
+        height={168}
+        punch={10}
+      />
       <img className={styles.projectImage} src={`./projects/${item.image}`} alt={item.name} />
       {item.starred && <img className={styles.star} src="star.svg" alt="starred-repo" />}
     </div>
